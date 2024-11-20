f126370e0ab41d2a6c777761470c163dd9bf9d2e
jasminlei <jasminleinonen@hotmail.com>
2024-11-20 08:35:07 +0200

    add maksukortti-mock and fix osta_lounas in kassapaate.py

---
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import Mock
+from kassapaate import Kassapaate, HINTA
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassa = Kassapaate()
+
+    def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 10
+
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_called_with(HINTA)
+
+    def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 4
+
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_not_called()
