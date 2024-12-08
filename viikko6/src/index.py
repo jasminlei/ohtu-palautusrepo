27085748e6a8c4d618ff1cf945640a1097f2c4a4
jasminlei <jasminleinonen@hotmail.com>
2024-12-08 10:23:55 +0200

    create All, Not and HasFewerThan classes for NHL reader

---
+++ viikko6/src/index.py
@@ -0,0 +1,25 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(HasAtLeast(5, "goals"), HasAtLeast(20, "assists"), PlaysIn("PHI"))
+
+    matcher2 = And(Not(HasAtLeast(2, "goals")), PlaysIn("NYR"))
+
+    matcher3 = And(HasFewerThan(2, "goals"), PlaysIn("NYR"))
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+    for player in stats.matches(matcher3):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
